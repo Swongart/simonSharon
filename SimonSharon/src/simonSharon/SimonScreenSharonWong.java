c1ea37df9b075da18db130d97f6d316a5c8ddd19
Sharon <Sharon@DESKTOP-1PNI6LC>
2017-01-11 21:45:38 -0500

    where are the errs?

--- SimonSharon/src/simonSharon/SimonScreenSharonWong.java
+++ SimonSharon/src/simonSharon/SimonScreenSharonWong.java
@@ -125,8 +125,8 @@ public class SimonScreenSharonWong extends ClickableScreen implements Runnable {
 		for(int i =0; i < numberOfButtons; i++){
 			final ButtonInterfaceSharonWong b = getAButton();
 			b.setColor(buttonColor[i]);
-			b.setX(100+(i*20));
-			b.setY(100+(i*20));
+			b.setX(150 + (int)(100*Math.cos(i*2*Math.PI/(numberOfButtons))));
+			b.setY(150 + (int)(100*Math.cos(i*2*Math.PI/(numberOfButtons))));
 			b.setAction(new Action(){
 				
 				public void act(){
